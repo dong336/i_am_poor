f6d117acea847dd33c1aa50ded853c71cb2ff320
kdw <50583568+dong336@users.noreply.github.com>
2024-11-30 16:23:21 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am poor'),
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          )
+        ),
+      ),
+    );
+  }
+}
